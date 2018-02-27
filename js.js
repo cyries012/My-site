28d11330f5aba540b2a51954394375b7393df640
Puch, Tomasz <tomasz.puch@tesco.com>
2018-02-27 19:22:31 +0000

    Refactor

--- js.js
+++ js.js
@@ -1,59 +1,64 @@
-$(document).ready(function(){
+function setBindings() {
+    $('#myNavbar a').click(function (event) {
+        var id = event
+                    .target
+                    .getAttribute('href')
+                    .slice(1);
+
+        $('html, body')
+            .animate({
+                scrollTop: $(document.getElementById(id)).position().top
+            }, 300);
+
+        return false;
+    });
+}
+
+function scrollActive() {
+    var ID = 0;
+    var POSITION = 1;
+    var positions = [
+        'homeSection',
+        'aboutSection',
+        'servicesSection',
+        'contactSection'
+    ].map(function (id) {
+        var $el = $(document.getElementById(id));
+        var offsetTop = $el.offset().top;
+        var outerHeight = $el.outerHeight();
+
+        return [id, [offsetTop, offsetTop + outerHeight]];
+    });
+    var currentPosition = document.body.scrollTop;
+    var heigth = window.innerHeight;
+    var baseOffset = $('.navbar').outerHeight();
+    var currentSections = positions.filter(function (position) {
+        return currentPosition + baseOffset >= position[POSITION][0] && currentPosition <= position[POSITION][1];
+    });
+    var currentSection = null;
+
+    if (currentSections.length) {
+        currentSection = currentSections[0][ID];
+    }
+
+    if (!currentPosition) {
+        return;
+    }
+    
+    $('#myNavbar [href^="#"]')
+        .parent()
+        .removeClass('active');
+    $('#myNavbar [href="#' + currentSection + '"]')
+        .parent()
+        .addClass('active');
+}
+
+$(document).ready(function () {
     setBindings();
     scrollActive();
 });
-
-function setBindings() {
-    $("li a").click(function(e){
-
-        e.preventDefault();
-        var sectionID = e.currentTarget.id + "Section";
-        
-
-       $("html").animate({scrollTop: $("#" + sectionID).offset().top}, 1000)
-    })
-}
-
-/*function setActive(){
-        $('ul.nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        });  }*/
-
-function scrollActive(){
-    var top = document.getElementById('header'); 
-    var homeSection = document.getElementById('homeSection').offsetTop;
-    var aboutSection = document.getElementById('aboutSection').offsetTop;
-    var servicesSection = document.getElementById('servicesSection').offsetTop;
-    var contactSection = document.getElementById('contactSection').offsetTop;
-    var scrollNow = window.pageYOffset;
-    var heigth = window.innerHeight;
-
-
-
-    
-    if (0 <= scrollNow && scrollNow < aboutSection)
-        {
-            $('ul.nav > li').removeClass('active');
-            $('.00').addClass('active'); 
-        }
-    else if (servicesSection > scrollNow && scrollNow >= aboutSection)
-        {
-            $('ul.nav > li').removeClass('active');
-            $('.11').addClass('active'); 
-        } 
-    else if ((contactSection - heigth) > scrollNow && scrollNow >= servicesSection)
-        {
-            $('ul.nav > li').removeClass('active');
-            $('.22').addClass('active');
-        } 
-    else if ((contactSection - heigth) < scrollNow)
-        {
-            $('ul.nav > li').removeClass('active');
-            $('.33').addClass('active');
-        } 
-
-    
-    window.addEventListener("scroll", scrollActive);
-}
+/**
+ * Po staremu to za kazdym odpaleniem scrollActive dodajesz
+ * event listenera do scrolla. To troche malo optymalne ;)
+ */
+window.addEventListener('scroll', scrollActive);
